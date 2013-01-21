84244fa292fca4cfb10bd68836bbb91ed8eb400c
Kent Rasmussen <kent.w.rasmussen@gmail.com>
2013-01-21 13:56:31 -0700

    Add some missed cases
    Nowhere near complete, I still have a lot to add.

--- Stats/src/com/github/earthiverse/stats/BlockFix.java
+++ Stats/src/com/github/earthiverse/stats/BlockFix.java
@@ -3,22 +3,31 @@ package com.github.earthiverse.stats;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 
+/*
+ * Fixes block data to personal preference
+ * (I don't think it's worthwhile to store what position the blocks were in,
+ * among other things)
+ * 
+ * Refer to http://www.minecraftwiki.net/wiki/Data_values
+ */
+
 public class BlockFix {
 	public static Block FixData(Block block) {
-		if ((block.getType().equals(Material.LEAVES) || block.getType().equals(
-				Material.SAPLING))
-				&& block.getData() >= 8) {
-			// Remove Leaves Decay and Sapling Growth Data
+		if (block.getType().equals(Material.LEAVES)) {
+			// Remove Leaves Decay Data
 			block.setData((byte) (block.getData() % 8));
 
-		} else if ((block.getType().equals(Material.TORCH) // TORCH
-				|| block.getType().equals(Material.REDSTONE_TORCH_OFF) // REDSTONE_TORCH_OFF
-				|| block.getType().equals(Material.REDSTONE_TORCH_ON) // REDSTONE_TORCH_ON
-		)) {
+		} else if (block.getType().equals(Material.SAPLING) && block.getData() >= 8) {
+			// Remove Sapling Growth Data
+			block.setData((byte) (block.getData() % 8));
+
+		} else if (block.getType().equals(Material.TORCH)
+				|| block.getType().equals(Material.REDSTONE_TORCH_OFF)
+				|| block.getType().equals(Material.REDSTONE_TORCH_ON)) {
 			// Remove Torch Position Data
 			block.setData((byte) 0);
 
-		} else if ((block.getType().equals(Material.WOOD_STAIRS)
+		} else if (block.getType().equals(Material.WOOD_STAIRS)
 				|| block.getType().equals(Material.COBBLESTONE_STAIRS)
 				|| block.getType().equals(Material.BRICK_STAIRS)
 				|| block.getType().equals(Material.SMOOTH_STAIRS)
@@ -26,16 +35,18 @@ public class BlockFix {
 				|| block.getType().equals(Material.SANDSTONE_STAIRS)
 				|| block.getType().equals(Material.SPRUCE_WOOD_STAIRS)
 				|| block.getType().equals(Material.BIRCH_WOOD_STAIRS)
-				|| block.getType().equals(Material.JUNGLE_WOOD_STAIRS)
+				|| block.getType().equals(Material.JUNGLE_WOOD_STAIRS)) {
 				//|| block.getType().equals(Material.QUARTZ_STAIRS)
-		)) {
 			// Remove Stairs Position Data
 			block.setData((byte) 0);
 
-		} else if (block.getTypeId() == 74 // GLOWING_REDSTONE_ORE
-		) {
+		} else if (block.getType().equals(Material.VINE)) {
+			// Remove Vine Position Data
+			block.setData((byte) 0);
+
+		} else if (block.getType().equals(Material.GLOWING_REDSTONE_ORE)) {
 			// Merge GLOWING_REDSTONE_ORE in to REDSTONE_ORE
-			block.setTypeId(73);
+			block.setType(Material.REDSTONE_ORE);
 
 		}
 
